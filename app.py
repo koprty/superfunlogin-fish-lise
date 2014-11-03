74681f67078b54a76993f2fd66a0e9d5ad511a48
koprty <your_email@example.com>
2014-11-03 13:17:48 -0500

    added session stuff

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import Connection
 from utils import adduser, authenticate
 
