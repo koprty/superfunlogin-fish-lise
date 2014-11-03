60b42476bfae7e6c327aea3de6c9647bdea3fbf7
Lise <koprty@gmail.com>
2014-11-03 14:04:27 -0500

    session n bug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for,session
 from pymongo import Connection
 from utils import adduser, authenticate
 
@@ -12,7 +12,7 @@ def home():
     else:
         u = request.form.get('uname',None)
         pswd = request.form.get('pswd',None)
-        valid_user = utils.authenticate(u,pswd)
+        valid_user = authenticate(u,pswd)
         if not(valid_user) and not u == None:
             return render_template("base.html",msg=u)
         else:
@@ -23,24 +23,26 @@ def home():
 def login():
     if request.method=="GET":
         #return "<h1>This is the login page</h1>"
-        return render_template("index.html", message = "")
+        return render_template("login.html", message = "")
     else:
         global cur_user
         global cur_name
-        username = request.form["username"]
-        password = request.form["password"]
-        button = request.form["b"]
-        if button == "Login":
-            validity = authenticate(username, password)
-            if validity:
-                cur_user = username
-                cur_name = get_name(username)
-                return redirect(url_for('loggedin1'))
-            else:
-                #return "<h1>This is the login page</h1>"
-                return render_template("index.html", message="Login unsuccessful")
+        username = request.form.get("uname",None)
+        password = request.form.get("pswd",None)
+        #button = request.form["b"]
+        #if button == "Login":
+        validity = authenticate(username, password)
+        if validity:
+            cur_user = username
+           ####### cur_name = get_name(username) #### IMPLEMENT IN UTILS
+            session['myuser'] = username
+            print session
+            return redirect(url_for('loggedin1'))
         else:
-            return redirect(url_for('register'))
+            #return "<h1>This is the login page</h1>"
+            return render_template("login.html", message="Login unsuccessful")
+        #else:
+            #return redirect(url_for('register'))
         
 @app.route("/logout")
 def logout():
@@ -52,20 +54,21 @@ def logout():
 def register():
     if request.method=="GET":
         #return "<h1>This is the register page</h1>"
-        return render_template("register.html", message="")
+        return render_template("register.html", rconf ="Getting register")
     else:
-        username = request.form["username"]
-        password = request.form["password"]
-        confirm = request.form["confirm_password"]
-        name = request.form["name"]
+        username = request.form.get("runame",None)
+        password = request.form.get("rpswd",None)
+        confirm = request.form.get("confirm_password",None)
+        name = request.form.get("nickname",None)
         if (confirm == password):
-            if utils.adduser(username,name,password):
-                return redirect(url_for('login'))
+            if adduser(username,name): #took out password -> reput when dictionary made into mongodb
+                return render_template("register.html", rconf="You have successfully registered.")
+                #return redirect(url_for('login'))
             else:
                 return render_template("register.html", rconf="Username taken. Try Again.")
         else:
             #return "<h1>This is the register page</h1>"
-            return render_template("register.html", message = "Password doesn't match confirmation.")
+            return render_template("register.html", rconf = "Password doesn't match confirmation.")
         
 @app.route("/info")
 def info():
@@ -74,16 +77,17 @@ def info():
 
 @app.route("/loggedin1")
 def loggedin1():
+    print session
     if "myuser" in session and not session["myuser"] == None :
         return render_template("myindex.html")
-    return "<h1>This is the first logged in page</h1>"
+    return "<h1>Failure to login: This is the first logged in page</h1>"
     #return render_template("loggedin1.html")
 
 @app.route("/loggedin2")
 def loggedin2():
-    if "myuser" in session and not session["myuser"] == None :
+    if "myuser" in session and not session["myuser"]==None :
         return "<h1> You have successfully logged in </h1>"#render_template("myindex.html")
-    return "<h1>This is the second logged in page</h1>"
+    return "<h1>Failure to login: This is the second logged in page</h1>"
     #return render_template("loggedin2.html")
 
 
@@ -91,5 +95,6 @@ def loggedin2():
 if __name__=="__main__":
     conn = Connection()
     db = conn['1258']
+    app.secret_key="*]%4WQ4ki[uUF!3pZcNbM8_4SsDFSEsd"
     app.debug = True
     app.run()
