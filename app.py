30939f39a5e08b2fc284caa9ea91b4fa0742bfa1
koprty <your_email@example.com>
2014-11-03 13:12:45 -0500

    merged app and cookies

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ from utils import adduser, authenticate
 app = Flask(__name__)
 
 @app.route("/", methods=["POST","GET"])
+@app.route("/home")
+def home():
+    if request.method=="GET":
+        return render_template("base.html")
+    else:
+        u = request.form.get('uname',None)
+        pswd = request.form.get('pswd',None)
+        valid_user = utils.authenticate(u,pswd)
+        if not(valid_user) and not u == None:
+            return render_template("base.html",msg=u)
+        else:
+            session['myuser'] = u
+            return redirect(url_for('loggedin1'))
+
 @app.route("/login", methods=["POST","GET"])
 def login():
     if request.method=="GET":
@@ -18,7 +32,7 @@ def login():
         button = request.form["b"]
         if button == "Login":
             validity = authenticate(username, password)
-            if validity == "Valid":
+            if validity:
                 cur_user = username
                 cur_name = get_name(username)
                 return redirect(url_for('loggedin1'))
@@ -30,7 +44,8 @@ def login():
         
 @app.route("/logout")
 def logout():
-    return "<h1>This is the logout page</h1>"
+    session.pop("myuser", None)
+    return "<h1>This is the logout page</h1>" #need to add logout page
     #return render_template("logout.html")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -44,11 +59,13 @@ def register():
         confirm = request.form["confirm_password"]
         name = request.form["name"]
         if (confirm == password):
-            adduser(username,name,password)
-            return redirect(url_for('login'))
+            if utils.adduser(username,name,password):
+                return redirect(url_for('login'))
+            else:
+                return render_template("register.html", rconf="Username taken. Try Again.")
         else:
             #return "<h1>This is the register page</h1>"
-            return render_template("register.html", message = "Password doesn't match confirmation")
+            return render_template("register.html", message = "Password doesn't match confirmation.")
         
 @app.route("/info")
 def info():
@@ -57,11 +74,15 @@ def info():
 
 @app.route("/loggedin1")
 def loggedin1():
+    if "myuser" in session and not session["myuser"] == None :
+        return render_template("myindex.html")
     return "<h1>This is the first logged in page</h1>"
     #return render_template("loggedin1.html")
 
 @app.route("/loggedin2")
 def loggedin2():
+    if "myuser" in session and not session["myuser"] == None :
+        return "<h1> You have successfully logged in </h1>"#render_template("myindex.html")
     return "<h1>This is the second logged in page</h1>"
     #return render_template("loggedin2.html")
 
